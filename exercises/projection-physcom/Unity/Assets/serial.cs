c56569a9493941016b32121fcfdb685acfe26333
Grayson Earle <graysonearle@gmail.com>
2018-04-10 14:05:31 -0400

    added unity

---
+++ exercises/projection-physcom/Unity/Assets/serial.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class serial : MonoBehaviour {
+
+	public string PortName = "/dev/cu.usbmodem1421";
+
+	SerialPort Stream = new SerialPort(PortName, 9600);
+
+	// Use this for initialization
+	void Start () {
+		Stream.Open();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		string val = Stream.ReadLine();
+
+		// now break it up into multiple values
+		string[] strArr = str.Split(",");
+
+	}
+}
